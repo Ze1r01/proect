bf2669243fa70ca1765f32456f26f5545016d0e1
Ze1r01 <ranelmugalamov152@gmail.com>
2025-05-21 22:40:31 +0300

    Create filesystem.h

---
+++ filesystem.h
@@ -0,0 +1,12 @@
+#ifndef FILESYSTEM_H
+#define FILESYSTEM_H
+
+#include <stdio.h>
+
+FILE* open_fs(const char* filename);
+void view_file(const char* fs_filename, const char* target_filename);
+void delete_file(const char* fs_filename, const char* target_filename);
+void add_new_file(const char* fs_filename, const char* new_filename, const char* content_lines[], int content_count);
+void modify_file(const char* fs_filename, const char* target_filename, const char* new_content[], int new_count);
+
+#endif
